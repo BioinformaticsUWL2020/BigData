cc8435554117b3d1de9361c4b6e7b71d0acab64a
SykesZachary <zachary.h.sykes@gmail.com>
2020-06-19 17:30:40 +0100

    GWAS Script for PD and LOAD
    Outputs Manhattan Plot, and QQ Plot
    
    Be sure to run the numericalisation script prior otherwise the genotypes will not be available for the GWAS() function
    
    Directory with plots as PNG added as well

--- PD_and_LOAD_GWAS_final_plot_output.R
+++ PD_and_LOAD_GWAS_final_plot_output.R
@@ -16,6 +16,8 @@ ext2_path      <- 'HapMap_and_Pheno_Files/'
 ext3_path      <- 'PD_Geno_by_Chrom/'
 ext4_path      <- 'LOAD_Geno_by_Chrom/'
 
+##### PD GWAS RUN #####
+
 # set dir to PD pheno an bring in traits
 setwd(paste0(top_level_path, ext2_path))
 pd_pheno <- read.table('pd_pheno.txt', header = TRUE)
@@ -30,7 +32,6 @@ Position   <- as.integer(000)
 Pheno      <- as.numeric(000)
 binding_df <- tibble(SNP, Chromosome, Position, Pheno)
 
-# PD GWAS RUN
 for(file in list.files(paste0(top_level_path, ext3_path))) {
   
   # Bring in PD genotype
@@ -41,7 +42,7 @@ for(file in list.files(paste0(top_level_path, ext3_path))) {
     pheno = pd_pheno,
     geno = geno,
     min.MAF = 0.05,
-    plot = TRUE
+    plot = FALSE
   )
   
   # Output marker 
@@ -53,10 +54,70 @@ for(file in list.files(paste0(top_level_path, ext3_path))) {
 }
 PD_GWAS_RESULTS <- binding_df[2:length(binding_df$SNP),]
 PD_GWAS_RESULTS <- PD_GWAS_RESULTS %>%
-  arrange(Chromosome)
+  arrange(Chromosome) %>%
+  filter(Pheno != 0.0000000)
 
+# PD Plots
 manhattanPlot(
   p = PD_GWAS_RESULTS$Pheno,
   chromosome = PD_GWAS_RESULTS$Chromosome,
-  signif = 5e-8
+  signif = 0.05
+)
+
+qqPlot(
+  pval = PD_GWAS_RESULTS$Pheno,
+  truncate = TRUE
+)
+
+##### LOAD GWAS RUN #####
+
+# set dir to PD pheno an bring in traits
+setwd(paste0(top_level_path, ext2_path))
+load_pheno <- read.table('load_pheno.txt', header = TRUE)
+
+# set to dir with LOAD geno for GWAS
+setwd(paste0(top_level_path, ext4_path))
+
+# Building binding df
+SNP        <- as.factor(000)
+Chromosome <- as.integer(000)
+Position   <- as.integer(000)
+Pheno      <- as.numeric(000)
+binding_df <- tibble(SNP, Chromosome, Position, Pheno)
+
+for(file in list.files(paste0(top_level_path, ext4_path))) {
+  
+  # Bring in LOAD genotype
+  geno <- read.table(file, header = TRUE, check.names = FALSE)
+  
+  # Run GWAS per chrom - rrBLUP
+  gwas <- GWAS(
+    pheno = load_pheno,
+    geno = geno,
+    min.MAF = 0.05,
+    plot = FALSE
+  )
+  
+  # Output marker 
+  print(paste0('Output for: ', file, ', complete'))
+  
+  # Bind together
+  binding_df <- rbind(binding_df, gwas)
+  
+}
+LOAD_GWAS_RESULTS <- binding_df[2:length(binding_df$SNP), ]
+LOAD_GWAS_RESULTS <- LOAD_GWAS_RESULTS %>%
+  arrange(Chromosome) %>%
+  filter(Pheno != 0.0000000)
+
+# LOAD Plots
+manhattanPlot(
+  p = LOAD_GWAS_RESULTS$Pheno,
+  chromosome = LOAD_GWAS_RESULTS$Chromosome,
+  signif = 0.05
+)
+
+qqPlot(
+  pval = LOAD_GWAS_RESULTS$Pheno,
+  truncate = TRUE
 )
